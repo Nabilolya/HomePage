fa9cf411ac91ed270ae9f7b3317c5778e0a44a4f
Nabil Bouhassoun <nabil.bouhassoun@hpe.com>
2023-03-17 16:16:31 +0100

    generate a Home-Landing Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeLandingComponent } from './home-landing/home-landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLandingComponent
   ],
   imports: [
     BrowserModule
